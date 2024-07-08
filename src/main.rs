fd9adc96465c1d0a2b99712751e3cc84ae738958
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-07-08 10:07:09 +0100

    Removed unnecessary enum

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-mod driver_info;
 mod led_data;
+mod driver_info;
 
-use chrono::DateTime;
-use driver_info::DRIVERS;
 use iced::alignment;
 use iced::executor;
 use iced::theme::{self, Theme};
 use iced::time;
-use iced::widget::{button, column, container, row, text};
+use iced::widget::{button, container, row, text, column};
 use iced::{
-    mouse,
-    widget::canvas::{self, Canvas, Frame, Path, Program},
-    Alignment, Application, Color, Command, Element, Length, Point, Renderer, Settings, Size,
-    Subscription,
+    Alignment, Application, Command, Element, Length, Settings, Subscription,
+    widget::canvas::{self, Canvas, Path, Frame, Program}, Color, Point, Size, mouse, Renderer
 };
-use led_data::{LedCoordinate, UpdateFrame, LED_DATA};
 use reqwest::Client;
 use serde::Deserialize;
-use std::f32;
 use std::time::{Duration, Instant};
+use led_data::{LedCoordinate, LED_DATA, UpdateFrame};
+use driver_info::DRIVERS;
+use std::f32;
+use chrono::DateTime;
 use tokio::time::sleep;
 
 #[derive(Debug, Deserialize)]
@@ -56,7 +54,6 @@ enum Message {
     Tick(Instant),
     Blink,
     DataFetched(Result<Vec<UpdateFrame>, String>),
-    FetchNextBatch,
 }
 
 impl Application for Race {
@@ -93,16 +90,7 @@ impl Application for Race {
                     self.state = State::Fetching;
                     self.update_frames.clear();
                     self.current_frame_index = 0;
-                    return Command::perform(
-                        fetch_driver_data(
-                            self.client.clone(),
-                            self.driver_numbers.clone(),
-                            0,
-                            3,
-                            120,
-                        ),
-                        Message::DataFetched,
-                    );
+                    return Command::perform(fetch_driver_data(self.client.clone(), self.driver_numbers.clone(), 0, 3, 120), Message::DataFetched);
                 }
                 State::Fetching => {
                     self.state = State::Idle;
@@ -127,8 +115,7 @@ impl Application for Race {
             Message::Blink => {
                 if !self.update_frames.is_empty() {
                     self.led_state = !self.led_state;
-                    self.current_frame_index =
-                        (self.current_frame_index + 1) % self.update_frames.len();
+                    self.current_frame_index = (self.current_frame_index + 1) % self.update_frames.len();
                 }
             }
             Message::DataFetched(Ok(new_frames)) => {
@@ -137,15 +124,7 @@ impl Application for Race {
                     self.state = State::Ticking {
                         last_tick: Instant::now(),
                     };
-                    return Command::perform(
-                        sleep_and_fetch_next(
-                            self.client.clone(),
-                            self.driver_numbers.clone(),
-                            3,
-                            120,
-                        ),
-                        Message::DataFetched,
-                    );
+                    return Command::perform(sleep_and_fetch_next(self.client.clone(), self.driver_numbers.clone(), 3, 120), Message::DataFetched);
                 } else {
                     self.state = State::Idle;
                 }
@@ -162,7 +141,9 @@ impl Application for Race {
     fn subscription(&self) -> Subscription<Message> {
         let tick = match self.state {
             State::Idle | State::Fetching => Subscription::none(),
-            State::Ticking { .. } => time::every(Duration::from_millis(10)).map(Message::Tick),
+            State::Ticking { .. } => {
+                time::every(Duration::from_millis(10)).map(Message::Tick)
+            }
         };
 
         let blink = match self.state {
@@ -175,89 +156,103 @@ impl Application for Race {
         Subscription::batch(vec![tick, blink])
     }
 
-    fn view(&self) -> Element<Message> {
-        if let State::Fetching = self.state {
-            return container(
-                text("DOWNLOADING DATA...")
-                    .size(50)
-                    .horizontal_alignment(alignment::Horizontal::Center)
-                    .vertical_alignment(alignment::Vertical::Center),
-            )
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .align_x(alignment::Horizontal::Center)
-            .align_y(alignment::Vertical::Center)
-            .into();
-        }
-
-        const MINUTE: u64 = 60;
-        const HOUR: u64 = 60 * MINUTE;
-
-        let seconds = self.duration.as_secs();
-
-        let duration = text(format!(
-            "{:0>2}:{:0>2}:{:0>2}.{:0>2}",
-            seconds / HOUR,
-            (seconds % HOUR) / MINUTE,
-            seconds % MINUTE,
-            self.duration.subsec_millis() / 10,
-        ))
-        .size(40);
-
-        let button = |label| {
-            button(text(label).horizontal_alignment(alignment::Horizontal::Center))
-                .padding(10)
-                .width(80)
-        };
-
-        let toggle_button = {
-            let label = match self.state {
-                State::Idle | State::Fetching => "Start",
-                State::Ticking { .. } => "Stop",
-            };
-
-            button(label).on_press(Message::Toggle)
-        };
-
-        let reset_button = button("Reset")
-            .style(theme::Button::Destructive)
-            .on_press(Message::Reset);
-
-        let duration_container = container(duration)
-            .padding(10)
-            .align_x(alignment::Horizontal::Left)
-            .align_y(alignment::Vertical::Bottom)
-            .width(Length::FillPortion(1));
-
-        let buttons_container = container(
-            row![
-                container(toggle_button).padding(10),
-                container(reset_button).padding(10)
-            ]
-            .align_items(Alignment::Center)
-            .spacing(20),
+fn view(&self) -> Element<Message> {
+    if let State::Fetching = self.state {
+        return container(
+            text("DOWNLOADING DATA...")
+                .size(50)
+                .horizontal_alignment(alignment::Horizontal::Center)
+                .vertical_alignment(alignment::Vertical::Center),
         )
-        .align_x(alignment::Horizontal::Right)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .align_x(alignment::Horizontal::Center)
+        .align_y(alignment::Vertical::Center)
+        .into();
+    }
+
+    const MINUTE: u64 = 60;
+    const HOUR: u64 = 60 * MINUTE;
+
+    let seconds = self.duration.as_secs();
+
+    let duration = text(format!(
+        "{:0>2}:{:0>2}:{:0>2}.{:0>2}",
+        seconds / HOUR,
+        (seconds % HOUR) / MINUTE,
+        seconds % MINUTE,
+        self.duration.subsec_millis() / 10,
+    ))
+    .size(40);
+
+    let button = |label| {
+        button(
+            text(label).horizontal_alignment(alignment::Horizontal::Center),
+        )
+        .padding(10)
+        .width(80)
+    };
+
+    let toggle_button = {
+        let label = match self.state {
+            State::Idle | State::Fetching => "Start",
+            State::Ticking { .. } => "Stop",
+        };
+
+        button(label).on_press(Message::Toggle)
+    };
+
+    let reset_button = button("Reset")
+        .style(theme::Button::Destructive)
+        .on_press(Message::Reset);
+
+    let duration_container = container(duration)
+        .padding(10)
+        .align_x(alignment::Horizontal::Left)
         .align_y(alignment::Vertical::Bottom)
         .width(Length::FillPortion(1));
 
-        let bottom_row = row![duration_container, buttons_container].width(Length::Fill);
+    let buttons_container = container(
+        row![
+            container(toggle_button).padding(10),
+            container(reset_button).padding(10)
+        ]
+        .align_items(Alignment::Center)
+        .spacing(20)
+    )
+    .align_x(alignment::Horizontal::Right)
+    .align_y(alignment::Vertical::Bottom)
+    .width(Length::FillPortion(1));
 
-        let canvas = Canvas::new(Graph {
-            data: LED_DATA.to_vec(),
-            led_state: self.led_state,
-            update_frames: self.update_frames.clone(),
-            current_frame_index: self.current_frame_index,
-        })
-        .width(Length::Fill)
-        .height(Length::Fill);
+    let bottom_row = row![
+        duration_container,
+        buttons_container
+    ]
+    .width(Length::Fill);
 
-        container(column![canvas, bottom_row].spacing(20))
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .padding(20)
-            .into()
-    }
+    let canvas = Canvas::new(Graph {
+        data: LED_DATA.to_vec(),
+        led_state: self.led_state,
+        update_frames: self.update_frames.clone(),
+        current_frame_index: self.current_frame_index,
+    })
+    .width(Length::Fill)
+    .height(Length::Fill);
+
+    container(
+        column![
+            canvas,
+            bottom_row
+        ]
+        .spacing(20)
+    )
+    .width(Length::Fill)
+    .height(Length::Fill)
+    .padding(20)
+    .into()
+}
+
+    
 
     fn theme(&self) -> Theme {
         Theme::Light
@@ -328,13 +323,7 @@ impl<Message> Program<Message> for Graph {
     }
 }
 
-async fn fetch_driver_data(
-    client: Client,
-    driver_numbers: Vec<u32>,
-    start_index: usize,
-    drivers_per_batch: usize,
-    entries_per_driver: usize,
-) -> Result<Vec<UpdateFrame>, String> {
+async fn fetch_driver_data(client: Client, driver_numbers: Vec<u32>, start_index: usize, drivers_per_batch: usize, entries_per_driver: usize) -> Result<Vec<UpdateFrame>, String> {
     let session_key = "9149";
     let start_time: &str = "2023-08-27T12:58:56.200";
     let end_time: &str = "2023-08-27T13:20:54.300";
@@ -342,9 +331,7 @@ async fn fetch_driver_data(
     let mut all_data: Vec<LocationData> = Vec::new();
 
     for chunk_start in (start_index..driver_numbers.len()).step_by(drivers_per_batch) {
-        for driver_number in &driver_numbers
-            [chunk_start..chunk_start + drivers_per_batch.min(driver_numbers.len() - chunk_start)]
-        {
+        for driver_number in &driver_numbers[chunk_start..chunk_start + drivers_per_batch.min(driver_numbers.len() - chunk_start)] {
             let mut fetched_entries = 0;
 
             while fetched_entries < entries_per_driver {
@@ -356,16 +343,9 @@ async fn fetch_driver_data(
                 let resp = client.get(&url).send().await.map_err(|e| e.to_string())?;
                 if resp.status().is_success() {
                     let data: Vec<LocationData> = resp.json().await.map_err(|e| e.to_string())?;
-                    let valid_data: Vec<LocationData> = data
-                        .into_iter()
-                        .filter(|d| d.x != 0.0 && d.y != 0.0)
-                        .collect();
+                    let valid_data: Vec<LocationData> = data.into_iter().filter(|d| d.x != 0.0 && d.y != 0.0).collect();
                     fetched_entries += valid_data.len();
-                    eprintln!(
-                        "Fetched {} entries for driver number {}",
-                        valid_data.len(),
-                        driver_number
-                    );
+                    eprintln!("Fetched {} entries for driver number {}", valid_data.len(), driver_number);
                     all_data.extend(valid_data);
                 } else {
                     eprintln!(
@@ -386,9 +366,7 @@ async fn fetch_driver_data(
     let mut current_frame: Option<UpdateFrame> = None;
 
     for data in all_data {
-        let timestamp = DateTime::parse_from_rfc3339(&data.date)
-            .map_err(|e| e.to_string())?
-            .timestamp_millis() as u64;
+        let timestamp = DateTime::parse_from_rfc3339(&data.date).map_err(|e| e.to_string())?.timestamp_millis() as u64;
         let x = data.x;
         let y = data.y;
         let driver_number = data.driver_number;
@@ -403,8 +381,7 @@ async fn fetch_driver_data(
 
         let color = driver.color;
 
-        let nearest_led = LED_DATA
-            .iter()
+        let nearest_led = LED_DATA.iter()
             .min_by(|a, b| {
                 let dist_a = ((a.x_led - x).powi(2) + (a.y_led - y).powi(2)).sqrt();
                 let dist_b = ((b.x_led - x).powi(2) + (b.y_led - y).powi(2)).sqrt();
@@ -418,17 +395,11 @@ async fn fetch_driver_data(
             } else {
                 update_frames.push(frame.clone());
                 current_frame = Some(UpdateFrame::new(timestamp));
-                current_frame
-                    .as_mut()
-                    .unwrap()
-                    .set_led_state(nearest_led.led_number, color);
+                current_frame.as_mut().unwrap().set_led_state(nearest_led.led_number, color);
             }
         } else {
             current_frame = Some(UpdateFrame::new(timestamp));
-            current_frame
-                .as_mut()
-                .unwrap()
-                .set_led_state(nearest_led.led_number, color);
+            current_frame.as_mut().unwrap().set_led_state(nearest_led.led_number, color);
         }
     }
 
@@ -439,19 +410,7 @@ async fn fetch_driver_data(
     Ok(update_frames)
 }
 
-async fn sleep_and_fetch_next(
-    client: Client,
-    driver_numbers: Vec<u32>,
-    drivers_per_batch: usize,
-    entries_per_driver: usize,
-) -> Result<Vec<UpdateFrame>, String> {
+async fn sleep_and_fetch_next(client: Client, driver_numbers: Vec<u32>, drivers_per_batch: usize, entries_per_driver: usize) -> Result<Vec<UpdateFrame>, String> {
     sleep(Duration::from_millis(334)).await;
-    fetch_driver_data(
-        client,
-        driver_numbers,
-        0,
-        drivers_per_batch,
-        entries_per_driver,
-    )
-    .await
+    fetch_driver_data(client, driver_numbers, 0, drivers_per_batch, entries_per_driver).await
 }
